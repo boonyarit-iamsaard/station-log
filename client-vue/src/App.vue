24fe0547f8cc7f67a031e41bb8e02da5fecfd0ad
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-01-17 22:28:55 +0700

    Add client-vue

---
+++ client-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <TheAppBar @drawerToggle="drawerToggle" />
+    <TheAppDrawer :drawer="drawer" />
+    <v-main></v-main>
+  </v-app>
+</template>
+
+<script>
+import TheAppBar from './components/TheAppBar';
+import TheAppDrawer from './components/TheAppDrawer';
+
+export default {
+  name: 'App',
+
+  components: {
+    TheAppBar,
+    TheAppDrawer
+  },
+  data: () => ({
+    drawer: false
+  }),
+  methods: {
+    drawerToggle() {
+      this.drawer = !this.drawer;
+      console.log(this.drawer);
+    }
+  }
+};
+</script>
---
+++ client-vue/src/components/TheAppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar color="primary" dark app>
+    <v-app-bar-nav-icon
+      class="hidden-md-and-up"
+      @click="$emit('drawerToggle')"
+    ></v-app-bar-nav-icon>
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'TheAppBar',
+  props: ['drawerToggle'],
+  data() {
+    return {};
+  },
+  computed: {
+    title() {
+      return this.$vuetify.breakpoint.mdAndUp
+        ? 'Bangkok Engineering - Station Log'
+        : 'Station Log';
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
