39c9871e743ea96f2eb2f96767e17b54cd663248
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-01-22 17:51:27 +0700

    :art: Improve skelton ui @clent-react

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider
+} from '@material-ui/core/styles';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
-
-import AppBarComponent from './shared/components/AppBarComponent';
-import AppDrawerComponent from './shared/components/AppDrawerComponent';
+import AppNavBar from './shared/components/AppNavBar';
 
 import './App.css';
 
-const drawerWidth = 240;
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#156665'
+    }
+  }
+});
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex'
   },
-  toolbar: theme.mixins.toolbar,
+
+  toolbar: {
+    height: 48
+  },
+
   content: {
     flexGrow: 1,
     padding: theme.spacing(3)
@@ -23,30 +33,16 @@ const useStyles = makeStyles(theme => ({
 
 const App = () => {
   const classes = useStyles();
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const drawerToggleHandler = () => {
-    setMobileOpen(!mobileOpen);
-  };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBarComponent
-        drawerWidth={drawerWidth}
-        drawerToggleHandler={drawerToggleHandler}
-        width="240"
-      />
-      <AppDrawerComponent
-        drawerWidth={drawerWidth}
-        mobileOpen={mobileOpen}
-        drawerToggleHandler={drawerToggleHandler}
-      />
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        Main content.
-      </main>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppNavBar>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          Main content.
+        </main>
+      </AppNavBar>
+    </ThemeProvider>
   );
 };
 
