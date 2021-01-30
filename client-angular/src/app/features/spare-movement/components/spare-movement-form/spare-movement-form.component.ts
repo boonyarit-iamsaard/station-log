bcd90b9df43b4091988b1c76d51e84d57afd2d9d
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-01-30 21:28:21 +0700

    :art: Update spare-movement-form @client-angular

--- client-angular/src/app/features/spare-movement/components/spare-movement-form/spare-movement-form.component.ts
+++ client-angular/src/app/features/spare-movement/components/spare-movement-form/spare-movement-form.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface Airline {
+  text: string;
+  value: string;
+}
+
+interface Store {
+  text: string;
+  value: string;
+}
+
+interface Names {
+  text: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-spare-movement-form',
@@ -7,25 +22,42 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./spare-movement-form.component.scss'],
 })
 export class SpareMovementFormComponent implements OnInit {
-  spareMovementForm = new FormGroup({
-    date: new FormControl(new Date()),
-    airline: new FormControl('CX'),
-    fltno: new FormControl(''),
-    acreg: new FormControl(''),
-    part: new FormControl(''),
-    desc: new FormControl(''),
-    serial: new FormControl('NIL'),
-    grn: new FormControl(''),
-    qty: new FormControl(0),
-    store: new FormControl(''),
-    usedBy: new FormControl(''),
-  });
+  spareMovementForm: FormGroup;
+  airlines: Airline[] = [
+    { text: 'ASL', value: 'ASL' },
+    { text: 'CX', value: 'CX' },
+    { text: 'LD', value: 'LD' },
+    { text: 'PR', value: 'PR' },
+  ];
+  stores: Store[] = [
+    { text: 'BKK', value: 'BKK' },
+    { text: 'BKK306', value: 'BKK306' },
+  ];
+  names: Names[] = [
+    { text: 'Boonyarit', value: 'Boonyarit' },
+    { text: 'Theerapong', value: 'Theerapong' },
+    { text: 'Totsapon', value: 'Totsapon' },
+  ];
 
   onSubmit() {
     console.log(this.spareMovementForm.value);
   }
 
-  constructor() {}
+  constructor() {
+    this.spareMovementForm = new FormGroup({
+      date: new FormControl(new Date()),
+      airline: new FormControl(this.airlines[1].value),
+      fltno: new FormControl('', Validators.required),
+      acreg: new FormControl(''),
+      part: new FormControl(''),
+      desc: new FormControl(''),
+      serial: new FormControl('NIL'),
+      grn: new FormControl(''),
+      qty: new FormControl(0),
+      store: new FormControl(this.stores[0].value),
+      usedBy: new FormControl(''),
+    });
+  }
 
   ngOnInit(): void {}
 }
--- client-angular/src/app/material/material.module.ts
+++ client-angular/src/app/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -25,6 +26,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatSelectModule,
     MatSidenavModule,
     MatSliderModule,
     MatSlideToggleModule,
