10f072b5490d46f1296e78851a8044402f08b0b2
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-02-24 15:05:41 +0700

    Update spare-movement-form

--- client-angular/src/app/features/spare-movement/components/spare-movement-form/spare-movement-form.component.ts
+++ client-angular/src/app/features/spare-movement/components/spare-movement-form/spare-movement-form.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-spare-movement-form',
   templateUrl: './spare-movement-form.component.html',
   styleUrls: ['./spare-movement-form.component.scss'],
 })
-export class SpareMovementFormComponent implements OnInit, OnChanges {
+export class SpareMovementFormComponent implements OnInit {
   spareMovementForm: FormGroup;
 
   airlines: string[] = ['ASL', 'CX', 'LD', 'PR'];
@@ -14,69 +19,62 @@ export class SpareMovementFormComponent implements OnInit, OnChanges {
   stores: string[] = ['BKK', 'BKK306'];
   names: string[] = ['Boonyarit', 'Theerapong', 'Totsapon'];
 
+  get acreg(): AbstractControl {
+    return this.spareMovementForm.controls.acreg;
+  }
+  get airline(): AbstractControl {
+    return this.spareMovementForm.controls.airline;
+  }
+  get grn(): AbstractControl {
+    return this.spareMovementForm.controls.grn;
+  }
+  get qty(): AbstractControl {
+    return this.spareMovementForm.controls.qty;
+  }
+
   onSubmit() {
     console.log(this.spareMovementForm.value);
-    // this.spareMovementForm.reset();
   }
 
   setPrefix(value: string) {
     switch (value) {
       case 'ASL':
-        this.spareMovementForm.controls['acreg'].patchValue('EI-');
+        this.acreg.patchValue('EI-');
         break;
       case 'PR':
-        this.spareMovementForm.controls['acreg'].patchValue('RP-C');
+        this.acreg.patchValue('RP-C');
         break;
       default:
-        this.spareMovementForm.controls['acreg'].patchValue('B-');
+        this.acreg.patchValue('B-');
     }
   }
 
-  onGrnInvalid() {
-    if (this.spareMovementForm.controls['grn'].hasError('required')) {
-      return 'GRN is required.';
-    }
-    return this.spareMovementForm.controls['grn'].hasError('minlength')
-      ? 'GRN must contain 10 digits.'
-      : null;
+  forceUpperCase(name: string, event) {
+    this.spareMovementForm.patchValue({
+      [name]: event.target.value.toUpperCase(),
+    });
   }
 
-  onQtyInvalid() {
-    if (this.spareMovementForm.controls['qty'].hasError('required')) {
-      return 'Qty is required.';
-    }
-    return this.spareMovementForm.controls['qty'].hasError('min')
-      ? 'Qty must not be 0.'
-      : null;
-  }
-
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.spareMovementForm = new FormGroup({
-      date: new FormControl(new Date()),
-      airline: new FormControl(this.airlines[1]),
-      fltno: new FormControl(null, Validators.required),
-      acreg: new FormControl('B-', Validators.required),
-      part: new FormControl(null, Validators.required),
-      desc: new FormControl(null, Validators.required),
-      type: new FormControl(this.types[0]),
-      serial: new FormControl('NIL', Validators.required),
-      grn: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(10),
-      ]),
-      qty: new FormControl(0, [Validators.required, Validators.min(1)]),
-      store: new FormControl(this.stores[0]),
-      usedBy: new FormControl(null, Validators.required),
+    this.spareMovementForm = this.formBuilder.group({
+      date: [new Date()],
+      airline: [this.airlines[1]],
+      fltno: [null, Validators.required],
+      acreg: ['B-', Validators.required],
+      part: [null, Validators.required],
+      desc: [null, Validators.required],
+      type: [this.types[0], Validators.required],
+      serial: ['NIL', Validators.required],
+      grn: [null, [Validators.required, Validators.minLength(10)]],
+      qty: [0, [Validators.required, Validators.min(1)]],
+      store: [this.stores[0]],
+      usedBy: [null, Validators.required],
     });
-    this.spareMovementForm.controls['airline'].valueChanges.subscribe(
-      (value) => {
-        console.log(value);
-        this.setPrefix(value);
-      }
-    );
-  }
 
-  ngOnChanges(): void {}
+    this.airline.valueChanges.subscribe((value) => {
+      this.setPrefix(value);
+    });
+  }
 }
